c4e3254ad89a320d1135da0a3913d81dfff480c1
jeanr84 <robin12du84@hotmail.fr>
2016-02-02 21:58:28 +0100

    Collection departement clean

--- initMongo.sh
+++ initMongo.sh
@@ -5,20 +5,21 @@ use openData
 db.dropDatabase()
 EOF
 
+mongoimport --db openData --collection chomages --type csv --file res/data/chomage/chomage.csv --fields departement,tauxChomage
+mongoimport --db openData --collection nomlistes --file res/data/nomListes/nomListe.json
+mongoimport --db openData --collection regionsavecdepartements --file res/data/regionsDepartements.json
+mongoimport --db openData --collection immigrationregions --type csv --file res/data/immigration/immigrationRegions.csv --fields _id,nbImmigres,poucentageImmigres
+mongoimport --db openData --collection immigrationdepartements --type csv --file res/data/immigration/immigrationDepartements.csv --fields _id,nbImmigres,poucentageImmigres
+mongoimport --db openData --collection naissancesdepartements --type csv --file res/data/naissances/naissancesDepartements.csv --fields _id,nbNaissances
+mongoimport --db openData --collection revenudepartements --type csv --file res/data/revenuMedian/revenuMedianDepartements.csv --fields _id,revenuMedian
+mongoimport --db openData --collection revenuregions --type csv --file res/data/revenuMedian/revenuMedianRegions.csv --fields _id,revenuMedian
+
 java -jar parser.jar res/data/regionales/csv/Reg_15_Resultats_Communes_T1.csv res/data/departements/populationDepartements.csv
 
-mongoimport --db openData --collection chomages --type csv --file res/data/chomage/chomage.csv --fields departement,tauxChomage
-
-mongoimport --db openData --collection nomlistes --file res/data/nomListes/nomListe.json
-
-mongoimport --db openData --collection regionsavecdepartements --file res/data/regionsDepartements.json
-
-mongoimport --db openData --collection immigrationregions --type csv --file res/data/immigration/immigrationRegions.csv --fields _id,nbImmigres,poucentageImmigres
-
-mongoimport --db openData --collection immigrationdepartements --type csv --file res/data/immigration/immigrationDepartements.csv --fields _id,nbImmigres,poucentageImmigres
-
-mongoimport --db openData --collection naissancesdepartements --type csv --file res/data/naissances/naissancesDepartements.csv --fields _id,nbNaissances
-
-mongoimport --db openData --collection revenudepartements --type csv --file res/data/revenuMedian/revenuMedianDepartements.csv --fields _id,revenuMedian
-
-mongoimport --db openData --collection revenuregions --type csv --file res/data/revenuMedian/revenuMedianRegions.csv --fields _id,revenuMedian
+mongo << 'EOF'
+use openData
+db.chomages.drop()
+db.immigrationdepartements.drop()
+db.naissancesdepartements.drop()
+db.revenudepartements.drop()
+EOF
