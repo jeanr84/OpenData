1f71df6a39728382582152addf09fd5e521db6e5
laura laporte <lauralaporte@macbook-pro-de-laura.home>
2016-02-02 16:34:44 +0100

    modif script

--- initMongo.sh
+++ initMongo.sh
@@ -2,14 +2,7 @@
 
 mongo << 'EOF'
 use openData
-db.chomages.drop()
-db.nomlistes.drop()
-db.regionsavecdepartements.drop()
-db.immigrationregions.drop()
-db.immigrationdepartements.drop()
-db.naissancesdepartements.drop()
-db.communes.drop()
-db.departements.drop()
+db.dropDatabase()
 EOF
 
 java -jar parser.jar res/data/regionales/csv/Reg_15_Resultats_Communes_T1.csv res/data/departements/populationDepartements.csv
@@ -25,3 +18,7 @@ mongoimport --db openData --collection immigrationregions --type csv --file res/
 mongoimport --db openData --collection immigrationdepartements --type csv --file res/data/immigration/immigrationDepartements.csv --fields _id,nbImmigres,poucentageImmigres
 
 mongoimport --db openData --collection naissancesdepartements --type csv --file res/data/naissances/naissancesDepartements.csv --fields _id,nbNaissances
+
+mongoimport --db openData --collection revenudepartements --type csv --file res/data/revenuMedian/revenuMedianDepartements.csv --fields _id,revenuMedian
+
+mongoimport --db openData --collection revenuregions --type csv --file res/data/revenuMedian/revenuMedianRegions.csv --fields _id,revenuMedian
